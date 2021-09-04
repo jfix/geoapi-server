01ec0826c6c4089aa144d9dddbd5686248aa2243
Jakob Fix <jakob.fix@gmail.com>
2021-09-04 18:41:51 +0200

    Add some tests to ignore non-routable addresses

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ const getInfo = (request, response) => {
         if (!request.query.ip) throw new Error('Missing IP address')
         const ip = request.query.ip
         if (!validate(ip)) throw new Error('Not a valid IP address')
-
+        if ('127.0.0.1' === ip || isPrivateIP(ip)) throw new Error('Non-routable IP address')
         open('./GeoLite2-City.mmdb').then((db) => {
             const res = db.get(ip)
             response.json(res)
@@ -16,6 +16,13 @@ const getInfo = (request, response) => {
         response.status(400).json({error: e.message})
     }
 }
+
+const isPrivateIP = (ip) => {
+    const parts = ip.split('.')
+    return parts[0] === '10' || 
+       (parts[0] === '172' && (parseInt(parts[1], 10) >= 16 && parseInt(parts[1], 10) <= 31)) || 
+       (parts[0] === '192' && parts[1] === '168')
+ };
 
 export {
     getInfo
